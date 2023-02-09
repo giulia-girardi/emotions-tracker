815a3a0cd6f71aff19e3913bea23ae9b7a124a64
Giulia Girardi <gg.giuliagirardi@gmail.com>
2023-02-09 14:53:22 +0100

    add home link in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     <div>
+        <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
         <Link to="/signup">Signup</Link>
     </div>
