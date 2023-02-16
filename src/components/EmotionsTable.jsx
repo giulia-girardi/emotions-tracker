4bff8ba234d9a06bbf98c9a9fd83af55de650855
saracsilva <saracarolinasilva@gmail.com>
2023-02-16 15:33:22 +0100

    created an array to get the days from the past 4 weeks

--- src/components/EmotionsTable.jsx
+++ src/components/EmotionsTable.jsx
@@ -4,20 +4,49 @@ import { UserAuthContext } from "../contexts/user.auth.context";
 function EmotionsTable() {
   const { user } = useContext(UserAuthContext);
   const currentUser = user.user;
-  const todayDate = new Date().toJSON().slice(0, 10);
+  /* const todayDate = new Date().toJSON().slice(0, 10); */
   /* filter to get the emotions of the day */
-  const todayValues = currentUser.emotions.filter((emotion) => {
+  /* const todayValues = currentUser.emotions.filter((emotion) => {
     if (emotion.date === todayDate) {
       return emotion;
     } else {
       return null;
     }
-  });
+  }); */
 
-  const arrAnger = [];
-  for (let i = 0; i < 5; i++) {
-    arrAnger.push();
+  const today = new Date();
+  let dateLimit = new Date(new Date().setDate(today.getDate() - 28))
+    .toJSON()
+    .slice(0, 10);
+  const todayDate = today.toJSON().slice(0, 10);
+  function getDays(startDate, endDate, steps = 1) {
+    const dateArray = [];
+    /*  let currentDate = new Date(startDate); */
+    console.log(new Date(todayDate) <= new Date(dateLimit));
+    while (new Date(todayDate) === new Date(dateLimit)) {
+      console.log("hello");
+      dateArray.push(new Date(todayDate).toJSON().slice(0, 10));
+      console.log(dateArray);
+      // Use UTC date to prevent problems with time zones and DST
+      todayDate.setUTCDate(todayDate.getUTCDate() + steps);
+    }
+
+    return dateArray;
   }
+  getDays();
+  const dates = getDays(todayDate, dateLimit);
+  console.log(dates);
+  /* function getEmotions() {
+    const arrAnger = [];
+    for (let i = 0; i < 28; i++) {
+      currentUser.emotions.filter((emotion) => {
+        if (emotion.date === todayDate) {
+          return emotion;
+        }
+      });
+       arrAnger.push(); 
+    }
+  } */
 
   return (
     <div className="flex p-2 items-center justify-center">
@@ -35,9 +64,9 @@ function EmotionsTable() {
           <tr className="gap-10">
             <td>Sadness</td>
             <td>
-              {todayValues
+              {/*  {todayValues
                 .map((emotion) => emotion.sadness)
-                .reduce((a, b) => a + b) / todayValues.length}
+                .reduce((a, b) => a + b) / todayValues.length} */}
             </td>
             <td>8</td>
 
@@ -49,9 +78,9 @@ function EmotionsTable() {
             <td>Anxiety</td>
             <td>
               {" "}
-              {todayValues
+              {/* {todayValues
                 .map((emotion) => emotion.anxiety)
-                .reduce((a, b) => a + b) / todayValues.length}
+                .reduce((a, b) => a + b) / todayValues.length} */}
             </td>
             <td>5</td>
 
@@ -63,9 +92,9 @@ function EmotionsTable() {
             <td>Anger</td>
             <td>
               {" "}
-              {todayValues
+              {/*  {todayValues
                 .map((emotion) => emotion.anger)
-                .reduce((a, b) => a + b) / todayValues.length}
+                .reduce((a, b) => a + b) / todayValues.length} */}
             </td>
             <td>5</td>
 
